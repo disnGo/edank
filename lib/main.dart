812d3e51cbde56c1af32e285c4f96392f697d72c
disnGo <baltabai.k@hotmail.com>
2023-07-28 16:31:43 +0600

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class BarChartData {
@@ -48,6 +48,13 @@ class BarChartData {
   final int outcome;
 
   BarChartData({required this.month, required this.income, required this.outcome});
+}
+
+class PieChartData{
+  final String category;
+  final int value;
+
+  PieChartData({required this.category, required this.value});
 }
 
 
@@ -61,14 +68,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
   
+  @override
+  HomeScreenState createState() => HomeScreenState();
+
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  final List<PieChartData> pieData = [
+    PieChartData(
+      category: 'Food',
+      value: 25,
+    ),
+    PieChartData(
+      category: 'Transport',
+      value: 15,
+    ),
+    PieChartData(
+      category: 'Rent',
+      value: 35,
+    ),
+    PieChartData(
+      category: 'Shopping',
+      value: 10,
+    ),
+    PieChartData(
+      category: 'Others',
+      value: 15,
+    ),
+];
+
+
   final List<BarChartData> data = [
       BarChartData(
         month: 'May',
@@ -97,6 +134,8 @@ class HomeScreen extends StatelessWidget {
       ),
   ];
 
+  bool _isBarChart = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,7 +148,7 @@ class HomeScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Friday, 29th of July',
+              'Saturday, 29th of July',
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
@@ -128,7 +167,7 @@ class HomeScreen extends StatelessWidget {
             const SizedBox(height: 16),
             Center(
               child: Text(
-                '94000.0',
+                '94000,00 ₸',
                 style: TextStyle(
                   fontSize: 48,
                   fontWeight: FontWeight.bold,
@@ -143,9 +182,9 @@ class HomeScreen extends StatelessWidget {
                 ElevatedButton.icon(
                   onPressed: () {},
                   icon: const Icon(Icons.add),
-                  label: const Text('Income\n25000'),
+                  label: const Text('Income\n25000,00 ₸'),
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.green[300],
+                    backgroundColor: Colors.green[300],
                     minimumSize:
                         Size(MediaQuery.of(context).size.width * 0.4, 80),
                     shape:
@@ -155,9 +194,9 @@ class HomeScreen extends StatelessWidget {
                 ElevatedButton.icon(
                   onPressed: () {},
                   icon: const Icon(Icons.remove),
-                  label: const Text('Expense\n11200'),
+                  label: const Text('Expense\n11200,00 ₸'),
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.red[300],
+                    backgroundColor: Colors.red[300],
                     minimumSize:
                         Size(MediaQuery.of(context).size.width * 0.4, 80),
                     shape:
@@ -166,113 +205,136 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 16.0),
-            SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              primaryYAxis: NumericAxis(),
-              series: <ChartSeries>[
-                ColumnSeries<BarChartData, String>(
-                    name: 'Income',
-                    dataSource: data,
-                    xValueMapper: (BarChartData sales, _) => sales.month,
-                    yValueMapper: (BarChartData sales, _) => sales.income,
-                    color: Colors.green[300]),
-                ColumnSeries<BarChartData, String>(
-                    name: 'Outcome',
-                    dataSource: data,
-                    xValueMapper: (BarChartData sales, _) => sales.month,
-                    yValueMapper: (BarChartData sales, _) => sales.outcome,
-                    color: Colors.red[300])
-              ],
-              legend: Legend(isVisible: true),
+            const SizedBox(height: 16.0),
+
+            IconButton( // this is a button widget with an icon
+              icon: _isBarChart 
+                  ? const Icon(Icons.bar_chart) 
+                  : const Icon(Icons.pie_chart), // this is the icon that we want to use
+              alignment: Alignment.topLeft,
+              iconSize: 32.0,
+              onPressed: () {
+                setState(() {
+                  _isBarChart = !_isBarChart; // this toggles the state variable when the button is pressed
+                });
+              },
             ),
 
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Recent Transactions',
-                  style:
-                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color:
-                      Colors.black),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child:
-                      const Text('View All', style:
-                      TextStyle(fontSize:
-                      16, fontWeight:
-                      FontWeight.bold, color:
-                      Colors.blue)),
-                ),
-              ],
-            ),
-            const SizedBox(height:
-            8),
-            Card(
-              shape:
-                  RoundedRectangleBorder(borderRadius:
-                  BorderRadius.circular(16)),
-              elevation:
-                  4,
-              child:
-                  ListTile(
-                leading:
-                    CircleAvatar(child:
-                    Icon(Icons.add, color:
-                    Colors.white), backgroundColor:
-                    Colors.green[300]),
-                title:
-                    Text('Income', style:
-                    TextStyle(fontSize:
-                    18, fontWeight:
-                    FontWeight.bold, color:
-                    Colors.black)),
-                subtitle:
-                    Text('Friday, 29th July', style:
-                    TextStyle(fontSize:
-                    14, color:
-                    Colors.grey[600])),
-                trailing:
-                    Text('15000', style:
-                    TextStyle(fontSize:
-                    18, fontWeight:
-                    FontWeight.bold, color:
-                    Colors.green[800])),
+
+            _isBarChart // this is a conditional expression that shows or hides the charts based on the state variable
+            ? SfCartesianChart( // this is the bar chart widget
+                primaryXAxis: CategoryAxis(),
+                primaryYAxis: NumericAxis(),
+                series: <ChartSeries>[
+                  ColumnSeries<BarChartData, String>(
+                      name: 'Income',
+                      dataSource: data,
+                      xValueMapper: (BarChartData sales, _) => sales.month,
+                      yValueMapper: (BarChartData sales, _) => sales.income,
+                      color: Colors.green[300]),
+                  ColumnSeries<BarChartData, String>(
+                      name: 'Outcome',
+                      dataSource: data,
+                      xValueMapper: (BarChartData sales, _) => sales.month,
+                      yValueMapper: (BarChartData sales, _) => sales.outcome,
+                      color: Colors.red[300])
+                ],
+                legend: Legend(isVisible: true),
+              )
+            : SfCircularChart( // this is the pie chart widget
+                series: <CircularSeries>[
+                  PieSeries<PieChartData, String>(
+                      dataSource: pieData,
+                      xValueMapper: (PieChartData data, _) => data.category,
+                      yValueMapper: (PieChartData data, _) => data.value,
+                      dataLabelSettings: const DataLabelSettings(isVisible: true))
+                ],
               ),
-            ),
-            Card(
-              shape:
-                  RoundedRectangleBorder(borderRadius:
-                  BorderRadius.circular(16)),
-              elevation:
-                  4,
-              child:
-                  ListTile(
-                leading:
-                    CircleAvatar(child:
-                    Icon(Icons.add, color:
-                    Colors.white), backgroundColor:
-                    Colors.green[300]),
-                title:
-                    Text('Income', style:
-                    TextStyle(fontSize:
-                    18, fontWeight:
-                    FontWeight.bold, color:
-                    Colors.black)),
-                subtitle:
-                    Text('Friday, 29th July', style:
-                    TextStyle(fontSize:
-                    14, color:
-                    Colors.grey[600])),
-                trailing:
-                    Text('2800', style:
-                    TextStyle(fontSize:
-                    18, fontWeight:
-                    FontWeight.bold, color:
 
-Colors.green[800])),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Recent Transactions',
+                style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color:
+                    Colors.black),
+              ),
+              TextButton(
+                onPressed: () {},
+                child:
+                    const Text('View All', style:
+                    TextStyle(fontSize:
+                    16, fontWeight:
+                    FontWeight.bold, color:
+                    Colors.blue)),
+              ),
+            ],
+          ),
+          const SizedBox(height:
+          8),
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius:
+                BorderRadius.circular(16)),
+            elevation:
+                4,
+            child:
+                ListTile(
+              leading:
+                  CircleAvatar(child:
+                  Icon(Icons.add, color:
+                  Colors.white), backgroundColor:
+                  Colors.green[300]),
+              title:
+                  Text('Income', style:
+                  TextStyle(fontSize:
+                  18, fontWeight:
+                  FontWeight.bold, color:
+                  Colors.black)),
+              subtitle:
+                  Text('Saturday, 29th July', style:
+                  TextStyle(fontSize:
+                  14, color:
+                  Colors.grey[600])),
+              trailing:
+                  Text('15000,00 ₸', style:
+                  TextStyle(fontSize:
+                  18, fontWeight:
+                  FontWeight.bold, color:
+                  Colors.green[800])),
+            ),
+          ),
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius:
+                BorderRadius.circular(16)),
+            elevation:
+                4,
+            child:
+                ListTile(
+              leading:
+                  CircleAvatar(child:
+                  Icon(Icons.add, color:
+                  Colors.white), backgroundColor:
+                  Colors.green[300]),
+              title:
+                  Text('Income', style:
+                  TextStyle(fontSize:
+                  18, fontWeight:
+                  FontWeight.bold, color:
+                  Colors.black)),
+              subtitle:
+                  Text('Friday, 28th July', style:
+                  TextStyle(fontSize:
+                  14, color:
+                  Colors.grey[600])),
+              trailing:
+                  Text('2800,00 ₸', style:
+                  TextStyle(fontSize:
+                  18, fontWeight:
+                  FontWeight.bold, color: Colors.green[800])),
               ),
             ),
           ],
